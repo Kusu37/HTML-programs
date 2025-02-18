3520c62b8ea745423613190f1074ae48105e07a0
Kusuma K <96782290+Kusu37@users.noreply.github.com>
2025-02-18 21:41:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function toggleDescription(item) {
+  const description = item.querySelector('.portfolio-description');
+  description.style.display = (description.style.display === 'block') ? 'none' : 'block';
+}
